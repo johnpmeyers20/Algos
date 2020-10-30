47c6a8c5575a7f3971bc1fceaffaf5002a3094b3
JP Meyers <johnpmeyers20@yahoo.com>
2020-10-30 10:46:32 -0600

    improvements to differentSquares. use SETS!

--- 10302020_CS_differentSquares.js
+++ 10302020_CS_differentSquares.js
@@ -2,6 +2,23 @@
 //calculate the number of unique 2 × 2 squares in it.
 
 function differentSquares(matrix) {
+  let uniq = new Set();
+  for (let i = 0; i < matrix.length-1; i++) {
+      for (let j = 0; j < matrix[0].length-1; j++) {
+          uniq.add(matrix[i][j].toString() + 
+          matrix[i][j+1].toString() + 
+          matrix[i+1][j].toString() + 
+          matrix[i+1][j+1].toString());
+      }
+  }
+  return uniq.size;
+}
+
+///////////////////////////////////////////////////////////////////////////
+/////////////////////////// first attempt /////////////////////////////////
+///////////////////////////////////////////////////////////////////////////
+
+function differentSquares(matrix) {
   const length = matrix.length;
   const width = matrix[0].length;
   
