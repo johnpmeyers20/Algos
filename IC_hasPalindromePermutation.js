670b3a9a6c4e912ce76c3948127abc1821b458f9
johnpmeyers20 <johnpmeyers20@yahoo.com>
2021-09-10 14:41:07 -0400

    IC_hasPalindromePermutation.js Third and most efficient answer

--- IC_hasPalindromePermutation.js
+++ IC_hasPalindromePermutation.js
@@ -1,3 +1,7 @@
+////////////////////////////////////////////////
+////////////// First Attempt ///////////////////
+////////////////////////////////////////////////
+
 function hasPalindromePermutation(theString) {
 
   // Check if any permutation of the input is a palindrome
@@ -21,4 +25,39 @@ function hasPalindromePermutation(theString) {
   const odd = parity.filter(i => i === false);
 
   return odd.length > 1 ? false : true;
+}
+
+////////////////////////////////////////////////
+////////////// Second Attempt //////////////////
+////////////////////////////////////////////////
+
+function hasPalindromePermutation(theString) {
+
+  // Check if any permutation of the input is a palindrome
+
+  let arr = theString.split('');
+  let letters = new Set(arr);
+  let quans = Array.from(letters).map(i => arr.filter(j => i === j).length);
+  let odds = quans.filter(i => i % 2 !== 0);
+
+  return odds.length > 1 ? false : true;
+}
+
+////////////////////////////////////////////////
+/////////////// Third Attempt //////////////////
+////////////////////////////////////////////////
+
+function hasPalindromePermutation(theString) {
+  let odds = new Set();
+
+  for (let char of theString) {
+    if (odds.has(char)) {
+      odds.delete(char);
+    }
+    else {
+      odds.add(char);
+    }
+  }
+
+  return odds.size > 1 ? false : true;
 }
